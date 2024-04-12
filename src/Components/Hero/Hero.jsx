f641734beaf998a369e17a0c8017a7800e3f0572
LuA1i <117823970+LuA1i@users.noreply.github.com>
2024-04-12 18:47:34 +0100

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return <div className="hero">
+    <div className="hero-text">
+      <h1>We Ensure better education for a better world</h1>
+      <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills and experiences needed to excel in the dynamic field of education</p>
+
+      <button className="btn"></button>
+    </div>
+  </div>
+}
+
+export default Hero
